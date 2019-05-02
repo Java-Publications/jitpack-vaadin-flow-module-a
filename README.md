8e0648af369c6ff05389375514525d2d2686f91f
Sven Ruppert <sven.ruppert@gmail.com>
2019-05-02 16:28:36 +0200

    added DemoComponent

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # jitpack-vaadin-flow-module-a
+
+
+## Demo 
+A demo based on the master branch is deployed 
+on heroku:
+
+[https://jitpack-vaadin-flow-module-a.herokuapp.com](https://jitpack-vaadin-flow-module-a.herokuapp.com)
+
--- src/main/java/org/rapidpm/vaadin/nano/pages/MainView.java
+++ src/main/java/org/rapidpm/vaadin/nano/pages/MainView.java
@@ -1,19 +1,21 @@
 package org.rapidpm.vaadin.nano.pages;
 
 import com.vaadin.flow.component.Composite;
-import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 import org.rapidpm.dependencies.core.logger.HasLogger;
 import org.rapidpm.vaadin.nano.MainLayout;
+import org.rapidpm.vaadin.nano.component.DemoComponent;
 
 @Route(value = MainView.NAV_MAIN_VIEW, layout = MainLayout.class)
 public class MainView
-    extends Composite<Div> implements HasLogger {
+    extends Composite<VerticalLayout>
+    implements HasLogger {
   public static final String NAV_MAIN_VIEW = "";
 
   public MainView() {
-    getContent().add(new Span("Page content"));
+    getContent().add(new Span("Page content"), new DemoComponent());
   }
 
 }
